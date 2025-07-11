92d9f1a3a987d8db5c248e718542791481aec693
JesusAnaya <mmk.xloonytoonx@gmail.com>
2025-07-11 09:52:35 -0600

    corrigiendo rutas

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     e.preventDefault();
     setError('');
     try {
-      const res = await axios.post('http://localhost:4000/api/auth/login', { email, password });
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, { email, password });
       login(res.data);
       if (res.data.user.role === 'paciente') {
         navigate('/paciente');
